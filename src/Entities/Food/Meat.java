b3fbff23540bceb7ef873680c1946b4896933951
merzhang <mer.zhang01@gmail.com>
2021-11-03 12:01:03 -0400

    food entities

--- src/Entities/Food/Meat.java
+++ src/Entities/Food/Meat.java
@@ -7,4 +7,7 @@ import java.util.HashMap;
 
 public class Meat extends Food{
 
+    public Meat(int sl, String name, int quantity, String unit) {
+        super(sl, name, quantity, unit);
+    }
 }
--- src/use_cases/FoodFactory.java
+++ src/use_cases/FoodFactory.java
@@ -4,21 +4,21 @@ import Entities.Food.*;
 
 public interface FoodFactory {
 
-    public default Food getFood(String foodType){
+    public default Food getFood(String foodType, String foodName, int sl, int quantity, String unit){
         if(foodType == null){
             return null;
         }
         if(foodType.equalsIgnoreCase("GRAINS")) {
-            return new Grains();
+            return new Grains(sl, foodName, quantity, unit);
         }
         else if(foodType.equalsIgnoreCase("MEATS")){
-            return new Meat();
+            return new Meat(sl, foodName, quantity, unit);
         }
         else if(foodType.equalsIgnoreCase("FRUITVEGI")) {
-            return new FruitVegi();
+            return new FruitVegi(sl, foodName, quantity, unit);
         }
         else if(foodType.equalsIgnoreCase("DAIRY")) {
-            return new Dairy();
+            return new Dairy(sl, foodName, quantity, unit);
         }
         return null;
     }
