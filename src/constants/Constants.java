6cee8cf361c9b142a675f16430859d8409cd0a25
Marco <yautin.ng8@gmail.com>
2021-11-10 16:38:45 -0500

    added command constants to Constants

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -1,4 +1,18 @@
 package constants;
+import commandline.*;
+
+import java.util.Hashtable;
 
 public class Constants {
+
+    public static final Hashtable<String, CommandExecute> COMMANDS_DIC =
+            new Hashtable<String, CommandExecute>();
+
+    static {
+        COMMANDS_DIC.put("crtuser", new CreateUserCmd());
+        COMMANDS_DIC.put("crtfood", new CreateFoodCmd());
+        COMMANDS_DIC.put("crtrecipe", new CreateRecipeCmd());
+        COMMANDS_DIC.put("srtfood", new SortFoodCmd());
+        COMMANDS_DIC.put("srtrecipes", new SortRecipeCmd());
+    }
 }
