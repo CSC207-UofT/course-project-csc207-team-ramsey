163fefd14b7ecac05f9d2c16d7750b48e62035b0
Marco <yautin.ng8@gmail.com>
2021-11-09 21:40:09 -0500

    Competed doctests for RecipeManager

--- src/use_cases/RecipeManager.java
+++ src/use_cases/RecipeManager.java
@@ -1,6 +1,5 @@
 package use_cases;
 
-import Entities.Kitchen;
 import Entities.Recipe;
 import Entities.User;
 
@@ -14,12 +13,12 @@ public class RecipeManager {
 
     /**
      *
-     * @param user
-     * @param servings
-     * @param name
-     * @param time
-     * @param steps
-     * @param ingredients
+     * @param user a user of the program
+     * @param servings the number of people the recipe serves
+     * @param name the name of the recipe
+     * @param time the time it takes to cook in minutes
+     * @param steps the instructions of the recipe
+     * @param ingredients the required ingredients and their quantities
      * @return boolean showing whether recipe was added
      */
     public boolean addNewRecipe(User user, int servings, String name, int time, String steps, HashMap<String, Float> ingredients){
@@ -38,8 +37,8 @@ public class RecipeManager {
 
     /**
      * remove a recipe from the user's kitchen
-     * @param user
-     * @param name
+     * @param user a user of the program
+     * @param name the name of the recipe
      * @return boolean showing whether recipes was removed
      */
     public boolean removeRecipe(User user, String name){
@@ -55,16 +54,15 @@ public class RecipeManager {
 
     /**
      * change Recipe name to a newName
-     * @param user
-     * @param currName
-     * @param newName
+     * @param user a user of the program
+     * @param currName the name of the recipe
+     * @param newName the new name of the recipe
      * @return boolean representing whether new name was set
      */
     public boolean changeRecipeName(User user, String currName, String newName) {
         ArrayList<Recipe> curRecipes = user.getKitchen().getRecipes();
-        for (int i = 0; i < curRecipes.size(); i++){
-            Recipe curRecipe = curRecipes.get(i);
-            if (curRecipe.getTitle().equals(currName)){
+        for (Recipe curRecipe : curRecipes) {
+            if (curRecipe.getTitle().equals(currName)) {
                 curRecipe.setTitle(newName);
                 return true;
             }
@@ -74,16 +72,15 @@ public class RecipeManager {
 
     /**
      * change recipe servings
-     * @param user
-     * @param recipeName
-     * @param newServings
+     * @param user a user of the program
+     * @param recipeName the name of the recipe
+     * @param newServings the new servings of the recipe
      * @return boolean value showing whether newServings was set
      */
     public boolean changeRecipeServings(User user, String recipeName, int newServings){
         ArrayList<Recipe> curRecipes = user.getKitchen().getRecipes();
-        for (int i = 0; i < curRecipes.size(); i++){
-            Recipe curRecipe = curRecipes.get(i);
-            if (curRecipe.getTitle().equals(recipeName)){
+        for (Recipe curRecipe : curRecipes) {
+            if (curRecipe.getTitle().equals(recipeName)) {
                 curRecipe.setServings(newServings);
                 return true;
             }
@@ -93,16 +90,15 @@ public class RecipeManager {
 
     /**
      * change recipe prepTime
-     * @param user
-     * @param recipeName
-     * @param newPrepTime
-     * @return
+     * @param user  a user of the program
+     * @param recipeName the name of the recipe
+     * @param newPrepTime the new preptime of the recipe
+     * @return boolean value depending on whether recipe preptime is changed
      */
     public boolean changeRecipePrepTime(User user, String recipeName, int newPrepTime){
         ArrayList<Recipe> curRecipes = user.getKitchen().getRecipes();
-        for (int i = 0; i < curRecipes.size(); i++){
-            Recipe curRecipe = curRecipes.get(i);
-            if (curRecipe.getTitle().equals(recipeName)){
+        for (Recipe curRecipe : curRecipes) {
+            if (curRecipe.getTitle().equals(recipeName)) {
                 curRecipe.setPrepTime(newPrepTime);
                 return true;
             }
@@ -112,16 +108,15 @@ public class RecipeManager {
 
     /**
      * change recipe instructions
-     * @param user
-     * @param recipeName
-     * @param newInstructions
-     * @return
+     * @param user a user of the program
+     * @param recipeName the name of the recipe
+     * @param newInstructions the new instructions of the recipe
+     * @return boolean value depending on whether recipe instructions are changed
      */
     public boolean changeRecipeSteps(User user, String recipeName, String newInstructions){
         ArrayList<Recipe> curRecipes = user.getKitchen().getRecipes();
-        for (int i = 0; i < curRecipes.size(); i++){
-            Recipe curRecipe = curRecipes.get(i);
-            if (curRecipe.getTitle().equals(recipeName)){
+        for (Recipe curRecipe : curRecipes) {
+            if (curRecipe.getTitle().equals(recipeName)) {
                 curRecipe.setInstructions(newInstructions);
                 return true;
             }
@@ -131,16 +126,15 @@ public class RecipeManager {
 
     /**
      * change the ingredients of the recipe
-     * @param user
-     * @param recipeName
-     * @param newIngredients
-     * @return
+     * @param user a user of the program
+     * @param recipeName the name of the recipe
+     * @param newIngredients the new ingredients of the recipe
+     * @return boolean value depending on whether recipe ingredients is changed
      */
     public boolean changeIngredients(User user, String recipeName, HashMap<String, Float> newIngredients){
         ArrayList<Recipe> curRecipes = user.getKitchen().getRecipes();
-        for (int i = 0; i < curRecipes.size(); i++){
-            Recipe curRecipe = curRecipes.get(i);
-            if (curRecipe.getTitle().equals(recipeName)){
+        for (Recipe curRecipe : curRecipes) {
+            if (curRecipe.getTitle().equals(recipeName)) {
                 curRecipe.setIngredients(newIngredients);
                 return true;
             }
