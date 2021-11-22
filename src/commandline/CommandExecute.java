1909fa0d5f0a800654876111b248cfdde5efc2ca
Marco <yautin.ng@mail.utoronto.ca>
2021-11-22 14:36:07 -0500

    removed controlCentre parameter from execute...

--- src/commandline/CommandExecute.java
+++ src/commandline/CommandExecute.java
@@ -6,6 +6,5 @@ import entities.User;
 import java.util.List;
 
 public interface CommandExecute {
-    String execute(ControlCentre receiver, List<String> arguments);
-
+    String execute(List<String> arguments);
 }
