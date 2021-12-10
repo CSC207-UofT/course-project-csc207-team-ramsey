b1b5635a07e2e62f7dfd767933f2b3896ca928c3
toshahenrya <tosha.henryadams@mail.utoronto.ca>
2021-12-09 22:04:05 -0500

    take out abstracts

--- src/controllers/KitchenControlCentre.java
+++ src/controllers/KitchenControlCentre.java
@@ -26,57 +26,20 @@ public class KitchenControlCentre extends ControlCentre {
         return add;
     }
 
-    public StringBuilder getFoodList(User user){
+    public StringBuilder getFoodList(User user) {
         return user.getKitchen().showAllFood();
     }
 
-    public StringBuilder getShoppingList(User user){
+    public StringBuilder getShoppingList(User user) {
         return user.getKitchen().showShoppingList();
     }
 
-    public StringBuilder getRecipeList(User user){
+    public StringBuilder getRecipeList(User user) {
         return user.getKitchen().showRecipes();
     }
 
-    public HashMap<String, Float> getRecipeIngredients(String recipeName, User user){
+    public HashMap<String, Float> getRecipeIngredients(String recipeName, User user) {
         Recipe recipe = shoppingListManager.getRecipeFromTitle(user, recipeName);
         return recipe.getIngredients();
     }
-
-    @Override
-    public String showFood(Food foodEntry) {
-        return null;
-    }
-
-    @Override
-    public Food createEntity(String s, String s1, int sl, int quantity, String s2) {
-        return null;
-    }
-
-    @Override
-    public Food findFood(String foodName, User user) {
-        return null;
-    }
-
-    @Override
-    public String removeFoodQuantity(String foodName, String quantity, User user) {
-        return null;
-    }
-
-    @Override
-    public String updateFood(String foodName, String updateField, String updateItem, User user) {
-        return null;
-    }
-
-    @Override
-    public String addFoodtoList(Food food, Kitchen kitchen) {
-        return null;
-    }
-
-    @Override
-    public String addFoodQuantity(String foodName, String quantity, User user) {
-        return null;
-    }
-
-
 }
