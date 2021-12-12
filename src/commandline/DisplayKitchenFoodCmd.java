29f057d8840471e381d8b4ca10da2107007c8198
toshahenrya <tosha.henryadams@mail.utoronto.ca>
2021-12-12 09:29:32 -0500

    Update DisplayKitchenFoodCmd

--- src/commandline/DisplayKitchenFoodCmd.java
+++ src/commandline/DisplayKitchenFoodCmd.java
@@ -4,26 +4,45 @@ import controllers.ControlCentre;
 import controllers.KitchenControlCentre;
 import entities.User;
 import entities.food.Food;
+import usecases.FoodFactory;
 
 import java.util.*;
 
-public class DisplayKitchenFoodCmd extends Command implements CommandExecute{
+    public class DisplayKitchenFoodCmd<T> extends Command<T> implements CommandExecute, FoodFactory {
 
-    public DisplayKitchenFoodCmd(ControlCentre receiver) {
-        super(0, 0, receiver);
-    }
+        public DisplayKitchenFoodCmd(T receiver) {
+            super(0, 0, receiver);
+        }
 
     @Override
     public String execute(List<String> arguments) {
-        ArrayList<Food> foodList = receiver.getUser().getKitchen().getFood();
-        KitchenControlCentre kitchenControlCentre = (KitchenControlCentre) receiver;
+        if (arguments.get(0) == "YES") {
+            ArrayList<Food> foodList = ((KitchenControlCentre) receiver).getUser().getKitchen().getFood();
 
-        if (foodList == null || foodList.isEmpty()){
-            return "You currently have no food in your kitchen.";
+            if (foodList == null || foodList.isEmpty()) {
+                return "You currently have no food in your kitchen.";
+            } else {
+                StringBuilder foods = new StringBuilder();
+                foods.append("You currently have the following food in your kitchen: " + "\n");
+                return ((KitchenControlCentre) receiver).getFoodList(((KitchenControlCentre) receiver).getUser()).toString();
+            }
         } else {
-            StringBuilder foods = new StringBuilder();
-            foods.append("You currently have the following food in your kitchen: " + "\n");
-            return kitchenControlCentre.getFoodList(receiver.getUser()).toString();
+            return "Ok, return to the main menu";
         }
     }
-}
+
+
+        @Override
+        public void initiate(Scanner s) {
+            System.out.println("Would you like to see all of the food in your kitchen? Input 'YES' or 'NO'.");
+            String response = s.nextLine();
+            List<String> args = new ArrayList<>();
+            if (response.equals("YES") || response.equals("NO")){
+                args.add(response);
+                System.out.println(execute(args));
+            } else {
+                System.out.println("I'm sorry, that response is invalid. Please, try again.");
+                }
+            }
+        }
+    }
