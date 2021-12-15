1b7fcf41ffc8173bcf1e67113745e5ea2e252509
Marco <yautin.ng@mail.utoronto.ca>
2021-12-15 17:23:23 -0500

    UI changes

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,6 +1,5 @@
 package UI;
 
-import commandline.RecipeCommands.DeleteRecipeCmd;
 import commandline.UserCommands.DeleteUser;
 import commandline.UserCommands.LoginRegisterUI;
 import commandline.UserCommands.SaveUserChangeCmd;
@@ -19,19 +18,19 @@ public class UI {
     private LoginController loginController;
     User user;
 
-    public UI(User user) throws Exception {
-
+    public UI() throws Exception {
+        LoginRegisterUI loginRegisterUI = new LoginRegisterUI();
+        this.user = loginRegisterUI.initiate();
         KitchenControlCentre kitchenControlCentre = new KitchenControlCentre(user);
-        UserControlCentre userControlCentre = new UserControlCentre();
         RecipeControlCentre recipeControlCentre = new RecipeControlCentre(user);
         FoodControlCentre foodControlCentre = new FoodControlCentre(user);
+        UserDetailsController userDetailsController = new UserDetailsController(user);
         this.s = new Scanner(System.in);
-        this.constants = new Constants(kitchenControlCentre, userControlCentre, recipeControlCentre, foodControlCentre);
+        this.constants = new Constants(kitchenControlCentre, userDetailsController, recipeControlCentre, foodControlCentre);
         this.constants.populateConstants();
     }
 
-    public void receiveCommands(){
-
+    public void receiveCommands() throws Exception {
         // list out possible commands
         System.out.println("Please, input one of the following commands to continue:");
         System.out.println("\t- exit");
@@ -56,8 +55,12 @@ public class UI {
                 }
             }
             if (command.equals("exit")){
+                new SaveUserChangeCmd().saveUserChanges(user);
                 break;
-            } else if (!success){
+            } else if (command.equals("deleteAccount")){
+                new DeleteUser().initiate(this.user);
+            }
+            else if (!success){
                 System.out.println("inputted command does not exist.\n");
             }
 
